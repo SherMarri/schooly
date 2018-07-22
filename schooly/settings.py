b17ffde8de2c1ee557893016b95a14b141e43fba
SherMarri <marribaloch128@gmail.com>
2018-07-22 19:08:34 +0500

    Fixed ALLOWED_HOSTS

--- schooly/settings.py
+++ schooly/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$wfz0!%!)o50%r-9!ts7p_4vzt&*$)fo!x1t4a49wuvhl$1irk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
